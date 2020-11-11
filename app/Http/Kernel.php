eab9de8f347c63d787a4e7e901c4cdf6ac8bc8cf
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2020-11-11 12:07:32 +0600

    php artisan make:middleware RoleMiddleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'Age' => \App\Http\Middleware\AgeMiddleware::class,
+        'Role' => \App\Http\Middleware\RoleMiddleware::class,
     ];
 
     /**
