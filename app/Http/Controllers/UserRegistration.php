728c14aa82af959a67f1cc796528a4917413991f
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2020-11-12 11:55:05 +0600

    get user form data

---
+++ app/Http/Controllers/UserRegistration.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserRegistration extends Controller
+{
+    public function postRegister(Request $request) {
+		//Retrieve the name input field
+		$name = $request->input('name');
+		echo 'Name: '.$name;
+		echo '<br>';
+
+		//Retrieve the username input field
+		$username = $request->username;
+		echo 'Username: '.$username;
+		echo '<br>';
+
+		//Retrieve the password input field
+		$password = $request->password;
+		echo 'Password: '.$password;
+   }
+}
--- routes/web.php
+++ routes/web.php
@@ -73,3 +73,12 @@ Route::get('/myclass','ImplicitController@index');
 Route::get('/foo/bar','UriController@index');
 
 
+Route::get('/register',function() {
+   return view('register');
+});
+Route::post('/user/register',array(
+	'uses'=>'UserRegistration@postRegister'
+));
+
+
+
